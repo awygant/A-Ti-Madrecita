2f802fc0662b2be9a7df31c3b0b1d76541d0ed0b
Anna Wygant <anna.wygant@gmail.com>
2015-04-22 22:19:02 -0700

    version 1

---
+++ includes/resources.php
@@ -0,0 +1,10 @@
+<meta name = "viewport" content = "width=device-width, initial-scale=1">
+
+<link href='http://fonts.googleapis.com/css?family=Berkshire+Swash' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Merriweather:400,400italic' rel='stylesheet' type='text/css'>
+<link href = "css/bootstrap.min.css" rel="stylesheet">
+<link href = "css/style.css" rel="stylesheet">
+
+
+<script type = "text/javascript" src = "js/jquery-2.1.3.min.js"></script>
+<script type = "text/javascript" src = "js/bootstrap.min.js"></script>
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>A Ti Madrecita</title>
+    <?php include ("includes/resources.php") ?>
+</head>
+<body>
+    <div class = "container-fluid text-center collage-banner slideLeft">
+        <div class = "container text-center vertical-center">
+            <h1 class = "berkshire big brown">&iexcl;Los Tigres del Norte Quieren Sus Fotos!</h1>
+            <div class = "flower f1"></div>
+            <div class = "flower f2"></div>
+            <div class = "flower f3"></div>
+        </div>
+    </div>
+    <div class = "container text-center">
+        <div class = "row text-left vert-padding">
+            <div class = "col-sm-6">
+                <div>
+                    <h2 class = "berkshire medium brown no-margin horiz-padding vert-padding">Estamos creando un video especial para todas las madrecitas. &iexcl;Necesitamos su ayuda!</h2>
+                </div>
+            </div>
+            <div class = "col-sm-6 merriweather brown">
+                <div class = "white-panel">
+                    <p class = "medium">&iexcl;Hola, amigos! Pedimos fotos de su madrecita para usar en nuestro video especial.</p>
+                    <p class = "medium">Entregue sus fotos con mamá hasta este domingo y los usamos en nuestro video especial para todas las mamás.</p>
+                    <!-- TODO: TRANSLATE HERE -->
+                    <p class = "medium">1. Create an Account</p>
+                    <p class = "medium">2. Entregue sus fotos</p>
+                    <p class = "medium">3. Comparta</p>
+                    <button class = "brown-bg berkshire medium">&iexcl;Empiece!</button>
+                    <div class = "vert-padding">
+                        <p>Y compre <i>A Ti Madrecita</i> aquí:</p>
+                        <!-- TODO: FILL IN THESE LINKS -->
+                        <p><a href = "" target = "_blank">iTunes</a> | <a href = "" target = "_blank">Amazon</a></p>
+                        <p><img class = "contained" width = "300" src = "img/madrecita-album.jpeg" alt = "A Ti Madrecita"/></p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class = "footer text-center vert-padding berkshire brown">
+        <a href = "http://www.lostigresdelnorte.com" target = "_blank">Los Tigres del Norte</a> |
+        <a href = "http://www.fotam.com" target = "_blank">Fotam</a>
+    </div>
+</body>
+</html>
