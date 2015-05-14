839bd702692da4b82bd7a31a9e78a68bc9c7126d
Anna Wygant <anna.wygant@gmail.com>
2015-05-14 11:36:57 -0700

    analytics
    Added Google Analytics tag

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
     <?php include ("includes/resources.php") ?>
 </head>
 <body>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-48726736-3', 'auto');
+    ga('send', 'pageview');
+
+</script>
     <div class = "container-fluid text-center collage-banner slideLeft">
         <div class = "container text-center vertical-center">
             <h1 class = "berkshire big brown">&iexcl;Los Tigres del Norte Quieren Sus Fotos!</h1>
